492d022eb6c5f6a29a567637d147255a2aa1d582
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-08-28 15:30:06 +0200

    Add files via upload

---
+++ Roll until 6/Roll until 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Roll_until_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            int diceThrow;
+            int diceResult = 0;
+            do
+            {
+                diceThrow = random.Next(1, 7);
+                Console.WriteLine($"You rolled: {diceThrow}");
+
+              diceResult += diceThrow;
+
+            }
+            while (diceThrow < 6);
+            Console.WriteLine($"Total score: {diceResult}");
+        }
+    }
+}
