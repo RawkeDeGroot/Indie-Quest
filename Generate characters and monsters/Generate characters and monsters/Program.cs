9222376299eca5d0c39c17b2519d875eba11a3b5
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-08-28 15:27:19 +0200

    Add files via upload

---
+++ Generate characters and monsters/Generate characters and monsters/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Generate_characters_and_monsters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            int strengthScore = 0;
+            int gelatinousCubeHP = 0;
+            int gelatinousCubeArmyHP = 0;
+
+            for (int strengthDice = 0; strengthDice < 3; strengthDice++)
+            {
+                int d6 = random.Next(1, 7);
+                strengthScore += d6;
+            }
+
+            for (int cubeHealthDice = 0; cubeHealthDice < 8; cubeHealthDice++)
+            {
+                int d10 = random.Next(1, 11);
+                gelatinousCubeHP += d10 + 5;
+            }
+
+            for (int cubeCount = 0; cubeCount < 100; cubeCount++)
+            {
+                for (int diceCount = 0; diceCount < 8; diceCount++)
+                {
+                    int d10 = random.Next(1, 11);
+                  gelatinousCubeArmyHP += d10 + 5;
+
+                }
+            }
+
+            Console.WriteLine($"A character with strength {strengthScore} was created!");
+            Console.WriteLine($"A gelatinous cube with {gelatinousCubeHP} HP appears!");
+            Console.WriteLine($"Dear gods, an army of 100 cubes descends upon us with a total of {gelatinousCubeArmyHP} HP. We are doomed!");
+        }
+    } 
+}
