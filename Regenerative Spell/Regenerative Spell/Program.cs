6ae1295ccdc6210542796000c0d1391a4221207c
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-08-28 15:29:26 +0200

    Add files via upload

---
+++ Regenerative Spell/Regenerative Spell/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Regenerative_Spell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            int warriorHP = random.Next(1, 101);
+            Console.WriteLine($"Warrior HP: {warriorHP}\nThe Regenerate spell is cast!");
+
+            while (warriorHP < 50)
+            {
+                Console.WriteLine($"Warrior HP: {warriorHP + 10}");
+                warriorHP += 10;
+            }
+
+            Console.WriteLine("The Regenerate Spell is complete!");
+        }
+    }
+}
