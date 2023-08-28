3a3998f15061865fd2faac2a6d979f55cb82f011
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-08-28 15:31:32 +0200

    Add files via upload

---
+++ Bowling/Bowling/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Net.WebRequestMethods;
+
+namespace Bowling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            
+            int knockedPins1 = random.Next(0, 11);
+            int pinsleft = 10 - knockedPins1;
+
+            int knockedPins2 = random.Next(0, pinsleft + 1);
+
+            int knockedPinsTotal = knockedPins1 + knockedPins2;
+
+            string scoreText = $"First Roll: {knockedPins1}\nSecond Roll: {knockedPins2}";
+
+            if (knockedPins1 == 10)
+            {
+                scoreText = $"First Roll: X";
+            }
+
+            if (knockedPinsTotal == 10 && knockedPins1 < 10)
+            {
+                scoreText = $"First Roll: {knockedPins1}\nSecond Roll: /";
+            }
+
+            Console.WriteLine(scoreText.Replace("0", "-"));
+            Console.WriteLine($"Knocked Pins: {knockedPinsTotal}");
+        }
+    }
+}
