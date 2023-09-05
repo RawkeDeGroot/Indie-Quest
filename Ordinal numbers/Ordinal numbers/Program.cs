9360bf89be036d0a069f55d1cab81255ac9ce9ca
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-09-05 14:10:25 +0200

    Add files via upload

---
+++ Ordinal numbers/Ordinal numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ordinal_numbers
+{
+    internal class Program
+    {
+        static string OrdinalNumber (int number)
+        {
+            int secondToLastDigit = 0;
+            
+            
+            int lastDigit = number % 10;
+            if (number >= 10) 
+            {
+                int n = number / 10;
+                secondToLastDigit = n % 10;
+            }
+
+            if (secondToLastDigit == 1)
+            {
+                return $"{number}th";
+
+            }
+            else if (lastDigit == 1)
+                return $"{number}st";
+
+            else if (lastDigit == 2) 
+                return $"{number}nd";
+
+            else if (lastDigit == 3) 
+                return $"{number}rd";
+            else return $"{number}th";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OrdinalNumber(121));
+        }
+    }
+}
