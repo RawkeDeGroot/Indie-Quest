027fcd1313d8348a66a7ee1dc59549ea834a7397
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-09-13 17:01:43 +0200

    Add files via upload

---
+++ Remember player name/Remember player name/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Remember_player_name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string playerNamePath = "player-name.txt";
+
+            if (File.Exists(playerNamePath))
+            {
+                string name = File.ReadAllText(playerNamePath);
+                Console.WriteLine($"Welcome back, {name}, let's continue!");
+            }
+
+            else
+            {
+                Console.WriteLine("Welcome to your biggest adventure yet!\n \nWhat is your name, traveler?");
+                string name = Console.ReadLine();
+                File.WriteAllText(playerNamePath, name);
+                Console.WriteLine($"Nice to meet you, {name}");
+            }
+        }
+    }
+}
