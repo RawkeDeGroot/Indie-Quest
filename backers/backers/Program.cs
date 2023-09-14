56c3003c46e6b79ae256e11b6c2fea251152c025
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-09-14 16:11:42 +0200

    Add files via upload

---
+++ backers/backers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace backers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerNamePath = "player-name.txt";
+            string backersFile = "backers.txt";
+            string[] backersArray = File.ReadAllLines(backersFile);
+
+            if (File.Exists(playerNamePath))
+            {
+                string playerName = File.ReadAllText(playerNamePath);
+                Console.WriteLine($"Welcome back {playerName}, let's continue!");
+                if ( backersArray.Contains(playerName)) 
+                {
+                    Console.WriteLine("You successfully enter Dr. Fred's secret laboratory and are greeted with a warm welcome for backing the game's Kickstarter!");
+                }
+                else
+                {
+                    Console.WriteLine("Unfortunately I cannot let you into Dr. Fred's secret laboratory.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Welcome to your biggest adventure yet!\n \nWhat is your name, traveler?");
+                string name = Console.ReadLine();
+                File.WriteAllText(playerNamePath, name);
+                Console.WriteLine($"Nice to meet you, {name}");
+            }
+        }
+    }
+}
