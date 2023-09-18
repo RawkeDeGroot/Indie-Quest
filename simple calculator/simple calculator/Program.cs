41c76764b0043cf495ccf72709d3e4718acf2705
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-09-18 14:25:26 +0200

    Add files via upload

---
+++ simple calculator/simple calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace simple_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Set the price: ");
+            string price = Console.ReadLine();
+            string[] Input = price.Split(' ');
+            int x;
+            int y;
+            x = Int32.Parse(Input[0]);
+            int result = 0;
+
+            if (Input.Length > 1) 
+            { 
+                y = int.Parse(Input[2]);
+                switch (Input[1]) 
+                {
+                    case "+":
+                        result = x + y;
+                        break;
+                    case "-":
+                        result = x - y;
+                        break;
+                    case "*":
+                    case "x":
+                        result = x * y;
+                        break;
+                    case "/":
+                        result = x / y;
+                        break;
+                }
+            }
+            else
+            {
+                result = x;
+            }
+            Console.WriteLine($"The price was set to {result}");
+        }
+    }
+}
