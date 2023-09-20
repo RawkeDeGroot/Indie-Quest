8891072ca59761c97427cbee31798733f4a964c0
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-09-20 16:25:19 +0200

    Add files via upload

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Enums
+{
+    internal class Program
+
+    {
+        enum Suits
+        {
+            Hearts,     
+            Diamonds,   
+            Clubs,      
+            Spades      
+        }
+        static void DrawAce(Suits suit)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            switch (suit)
+            {
+                case Suits.Hearts:
+                    {
+                        Console.WriteLine($"╭───────╮");
+                        Console.WriteLine($"│A      │");
+                        Console.WriteLine($"│♥      │");
+                        Console.WriteLine($"│   ♥   │");
+                        Console.WriteLine($"│      ♥│");
+                        Console.WriteLine($"│      A│");
+                        Console.WriteLine($"╰───────╯");
+                        return;
+                    }
+                case Suits.Diamonds:
+                    {
+                        Console.WriteLine($"╭───────╮");
+                        Console.WriteLine($"│A      │");
+                        Console.WriteLine($"│♦      │");
+                        Console.WriteLine($"│   ♦   │");
+                        Console.WriteLine($"│      ♦│");
+                        Console.WriteLine($"│      A│");
+                        Console.WriteLine($"╰───────╯");
+                        return;
+                    }
+                case Suits.Clubs:
+                    {
+                        Console.WriteLine($"╭───────╮");
+                        Console.WriteLine($"│A      │");
+                        Console.WriteLine($"│♣      │");
+                        Console.WriteLine($"│   ♣   │");
+                        Console.WriteLine($"│      ♣│");
+                        Console.WriteLine($"│      A│");
+                        Console.WriteLine($"╰───────╯");
+                        return;
+                    }
+                case Suits.Spades:
+                    {
+                        Console.WriteLine($"╭───────╮");
+                        Console.WriteLine($"│A      │");
+                        Console.WriteLine($"│♠      │");
+                        Console.WriteLine($"│   ♠   │");
+                        Console.WriteLine($"│      ♠│");
+                        Console.WriteLine($"│      A│");
+                        Console.WriteLine($"╰───────╯");
+                        return;
+                    }
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            DrawAce(Suits.Spades);
+        }
+    }
+}
