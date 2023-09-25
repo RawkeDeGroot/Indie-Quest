86870a3977cc09b70af3acb730daaa2a10fbb5f4
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-09-25 15:49:34 +0200

    Add files via upload

---
+++ random number monsters/random number monsters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace random_number_monsters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int[] levels = new int[100];
+            Console.Write($"Number of monsters in levels:");
+            for (int level  = 0; level < 100; level++) 
+            {
+                levels[level] = random.Next(1, 51);
+            }
+            Array.Sort(levels);
+            Console.Write(string.Join(", ", levels));
+            Console.WriteLine();
+        }
+    }
+}
