3413a7659eaf6c9b89fab849414e9fb0896e73ff
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-09-28 13:47:31 +0200

    Add files via upload

---
+++ Fractoral/Fractoral/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fractoral
+{
+    internal class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n == 0) return 1; 
+            else return n * Factorial(n - 1);
+        }
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(1));
+            Console.WriteLine(Factorial(5));
+            Console.WriteLine(Factorial(10));
+        }
+    }
+}
