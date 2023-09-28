0d7c1002d51d8ab5688743b3bf25924b7d7d83c8
RawkeDeGroot <38318799+RawkeDeGroot@users.noreply.github.com>
2023-09-28 13:27:37 +0200

    Add files via upload

---
+++ party shuffle/party shuffle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace party_shuffle
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        
+        static string partyShuffle(List <string> items) 
+        { 
+            for (int i = items.Count - 1; i > 0; i--) 
+            { 
+                int j = random.Next(i + 1);
+                object temp = items[i];
+                items[i] = items[j];
+                items[j] = (string)temp;
+            }
+            return string.Join(", ", items);
+        }
+        
+        static void Main(string[] args)
+        {
+            var names = new List<string>() { "Allie", "Ben", "Claire", "Dan", "Eleanor" };
+            Console.Write("Signed up participants: ");
+            Console.WriteLine(string.Join(", ", names));
+            Console.WriteLine("\nGenerating starting order...\n");
+            Console.Write("Starting order: ");
+            Console.WriteLine(partyShuffle(names));
+        }
+    }
+}
